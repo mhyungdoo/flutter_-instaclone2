e7d97cbd5bb902dd300e2c3c16d17119731c1ae7
mhyungdoo <mhyungdoo@gmail.com>
2020-08-04 14:13:15 +0900

    Instagram Clone Coding Sample - 로그인화면 구현

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,13 +1,20 @@
 import 'dart:async';
 
 import 'package:chapter10/root_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import 'tab_page.dart';
 
 class LoginPage extends StatelessWidget {
+  // 구글 로그인을 위한 객체
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  // firebase 인증 정보를 가지는 객체
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
 
   @override
   Widget build(BuildContext context) {
